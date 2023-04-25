9e02e9b8187fbbc79774ce2947abb9119c9e0124
cabbage556 <liberty556@gmail.com>
2023-04-25 16:27:06 +0900

    feat: CreateAnswerDTO 필수값 유효성 검사 추가
    fix-#34

--- src/apis/answers/dto/create-answer.dto.ts
+++ src/apis/answers/dto/create-answer.dto.ts
@@ -1,4 +1,19 @@
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateAnswerDTO {
+	@IsNotEmpty({
+		message: '내용을 입력해주세요.',
+	})
+	@IsString({
+		message: '문자열을 입력해주세요.',
+	})
 	contents: string;
+
+	@IsNotEmpty({
+		message: '게시글 id를 입력해주세요.',
+	})
+	@IsInt({
+		message: '정수를 입력해주세요.',
+	})
 	boardId: number;
 }
