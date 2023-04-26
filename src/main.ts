c7a5fc43e6a5fe940cdd4f8fca4538408268ddb1
HUMBLE25 <wlsrua1007@gmail.com>
2023-04-26 13:40:31 +0900

    Fix deployment cors에러 수정

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
 		origin: [
-			'http://localhost:3000',
-			'http://127.0.0.1:3000',
+			'http://localhost:3000', //
+			'http://127.0.0.1:3000', //
 			'https://need-romance.site', //
 		],
 	});
