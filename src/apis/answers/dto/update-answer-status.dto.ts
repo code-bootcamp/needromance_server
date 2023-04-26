722bcbafe92923bef52113222532a39ccdfb2296
cabbage556 <liberty556@gmail.com>
2023-04-26 11:46:52 +0900

    fix: UpdateAnswerStatusDTO 데이터 유효성 검사 추가
    fix-#39

--- src/apis/answers/dto/update-answer-status.dto.ts
+++ src/apis/answers/dto/update-answer-status.dto.ts
@@ -1,4 +1,19 @@
+import { IsBoolean, IsInt, IsNotEmpty } from 'class-validator';
+
 export class UpdateAnswerStatusDTO {
+	@IsNotEmpty({
+		message: '게시글 id를 입력해주세요.',
+	})
+	@IsInt({
+		message: '게시글 id를 정수로 입력해주세요.',
+	})
 	boardId: number;
+
+	@IsNotEmpty({
+		message: '채택 여부를 입력해주세요.',
+	})
+	@IsBoolean({
+		message: '채택 여부를 true 또는 false로 입력해주세요.',
+	})
 	status: boolean;
 }
