8474da9368d2b41e64c5ad75cab720c365d2ca03
HUMBLE25 <wlsrua1007@gmail.com>
2023-04-27 01:04:47 +0900

    Fix: main.ts cors 에러 해결을 위한 수정

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
-		origin: [
-			'http://localhost:3000', //
-			'http://127.0.0.1:3000', //
-			'http://need-romance.site',
-			'https://need-romance.site', //
-		],
+		credentials: true,
+		origin: 'http://localhost:3000', //
 	});
 	await app.listen(3100, () => {
 		console.log('💞💞💞로맨스가 필요해💞💞💞');
