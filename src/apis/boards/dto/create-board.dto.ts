7e568d469a3c0b5497b26de71ac9f88e3d2adca0
cabbage556 <liberty556@gmail.com>
2023-04-28 12:29:20 +0900

    docs: 게시글 DTO 명세 작성
    docs-#74

--- src/apis/boards/dto/create-board.dto.ts
+++ src/apis/boards/dto/create-board.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateBoardDTO {
+	@ApiProperty({
+		example: '연애 상담이 필요합니다.',
+		description: '게시글 제목(문자열)',
+	})
 	@IsNotEmpty({
 		message: '제목을 입력해주세요.',
 	})
@@ -9,6 +14,10 @@ export class CreateBoardDTO {
 	})
 	title: string;
 
+	@ApiProperty({
+		example: '지금 여자친구와 싸우고 아무 말도 하지 않는 상황...',
+		description: '게시글 내용(문자열)',
+	})
 	@IsNotEmpty({
 		message: '내용을 입력해주세요.',
 	})
@@ -17,5 +26,10 @@ export class CreateBoardDTO {
 	})
 	contents: string;
 
+	@ApiProperty({
+		type: [String],
+		example: `['#연애', '#상담']`,
+		description: '게시글 해시태그(문자열 배열)',
+	})
 	hashtags?: string[];
 }
