ccda0d86b70ad814aab8d16279d43784fc256763
cabbage556 <liberty556@gmail.com>
2023-04-28 12:14:52 +0900

    docs: Swagger 도큐먼트를 애플리케이션에 적용
    docs-#74

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerDocumentConfig } from './apis/swagger.document';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -9,6 +11,11 @@ async function bootstrap() {
 		credentials: true,
 		origin: process.env.FRONTEND_URL, //
 	});
+
+	const swaggerConfig = new SwaggerDocumentConfig().initializeOptions();
+	const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+	SwaggerModule.setup('api', app, swaggerDocument);
+
 	await app.listen(3100, () => {
 		console.log('💞💞💞로맨스가 필요해💞💞💞');
 	});
