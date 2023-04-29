10659b4c3c7a150931c9d06ef9552a5d05d81439
HUMBLE25 <wlsrua1007@gmail.com>
2023-04-29 15:50:14 +0900

    Feat: POST consult/question 불완전한 응답결과를 완전한 문장으로 응답할 수 있도록 수정

--- src/apis/consult/consult.service.ts
+++ src/apis/consult/consult.service.ts
@@ -6,26 +6,35 @@ import { IConsultServiceAnswer } from './interface/consult-service.interface';
 export class ConsultService {
 	async answer({ req, res }: IConsultServiceAnswer): Promise<string> {
 		const configuration = new Configuration({
+			organization: process.env.OPENAI_ORGANIZATION,
 			apiKey: process.env.OPENAI_KEY,
 		});
 		const openAi = new OpenAIApi(configuration);
-		const response = await openAi.createCompletion({
-			model: 'text-davinci-003',
-			prompt: `${req.body.question}`,
-			suffix: '대화 상대는 심리치료가 필요한 사람이야,한국어로 답해줘',
-			temperature: 1,
-			top_p: 0.4,
-			// stream: true,
-			best_of: 1,
-			// echo: false,
-			frequency_penalty: 0.0,
-			presence_penalty: 0.6,
-			stop: [' Human:', ' AI:'],
-			max_tokens: 1000,
-		});
+		try {
+			const completion = await openAi.createCompletion({
+				model: 'text-davinci-003',
+				prompt: `${req.body.question}`,
+				top_p: 0.2,
+				n: 1,
+				echo: true,
+				best_of: 1,
 
-		console.log(response.data.choices[0].text);
-		res.send(response.data.choices[0].text);
-		return response.data.choices[0].text;
+				frequency_penalty: 0.0,
+				presence_penalty: 0.6,
+				stop: [' Human:', ' AI:'],
+				max_tokens: 500,
+			});
+
+			console.log(completion.data.choices[0]);
+			res.send(completion.data.choices[0].text);
+			return completion.data.choices[0].text;
+		} catch (error) {
+			if (error.response) {
+				console.log(error.response.status);
+				console.log(error.response.data);
+			} else {
+				console.log(error.message);
+			}
+		}
 	}
 }
